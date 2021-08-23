344bc1526bbe48c2ef8228b84db569509964b9b1
Yearning <Yearning.dev@gmail.com>
2021-08-23 11:50:30 +0100

    Created the game class that handles the window and game loop.

---
+++ tetris-raylib/Game.cpp
@@ -0,0 +1,39 @@
+#include "Game.h"
+#include <raylib.h>
+#include <assert.h>
+Game::Game(int width, int height, std::string title)
+{
+	assert(!GetWindowHandle());	//If assertion triggers : Window is already opened
+	InitWindow(width, height, title.c_str());
+}
+
+Game::~Game() noexcept
+{
+	assert(GetWindowHandle()); //If assertion triggers : Window is already closed
+	CloseWindow();
+}
+
+bool Game::GameShouldClose() const
+{
+	return WindowShouldClose();
+}
+
+void Game::Tick()
+{
+	BeginDrawing();
+	Update();
+	Draw();
+	EndDrawing();
+}
+
+void Game::Update()
+{
+}
+
+void Game::Draw()
+{
+	ClearBackground(RAYWHITE);
+}
+
+
+
---
+++ tetris-raylib/Game.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+class Game
+{
+public:
+	Game(int width, int height, std::string title);
+	Game(const Game& other) = delete;
+	Game& operator=(const Game& other) = delete;
+	~Game() noexcept;
+	bool GameShouldClose() const;
+	void Tick();
+private:
+	void Update();
+	void Draw();
+};
