59ccd0b027c405b67941bd63087b56b1b2fa4ff3
Yearning <Yearning.dev@gmail.com>
2021-09-20 18:00:00 +0100

    Made the Vec2 class constexpr compatible.

--- tetris-raylib/Vec2.h
+++ tetris-raylib/Vec2.h
@@ -5,57 +5,57 @@ class Vec2
 {
 public:
 	Vec2() = default;
-	Vec2(T x, T y)
+	constexpr Vec2(T x, T y)
 		:
 		x(x),
 		y(y)
 	{}
-	T GetX() const {return x;};
-	T GetY() const {return y;};
-	void SetX(T x_in) { x = x_in;};
-	void SetY(T y_in) { y = y_in;};
+	constexpr T GetX() const {return x;};
+	constexpr T GetY() const {return y;};
+	constexpr void SetX(T x_in) { x = x_in;};
+	constexpr void SetY(T y_in) { y = y_in;};
 public:
-	bool operator==(const Vec2& rhs) const
+	constexpr bool operator==(const Vec2& rhs) const
 	{
 		return (x == rhs.x && y == rhs.y);
 	}
-	bool operator!=(const Vec2& rhs) const
+	constexpr bool operator!=(const Vec2& rhs) const
 	{
 		return !(*this == rhs);
 	}
-	Vec2 operator+(const Vec2& rhs) const
+	constexpr Vec2 operator+(const Vec2& rhs) const
 	{
 		return {x + rhs.x, y + rhs.y};
 	}
-	Vec2 operator+(const int rhs) const
+	constexpr Vec2 operator+(const int rhs) const
 	{
 		return {x + rhs, y + rhs};
 	}
-	Vec2& operator+=(const Vec2& rhs)
+	constexpr Vec2& operator+=(const Vec2& rhs)
 	{
 		 return *this = *this + rhs;
 	}
-	Vec2 operator-(const Vec2& rhs) const
+	constexpr Vec2 operator-(const Vec2& rhs) const
 	{
 		return {x - rhs.x, y - rhs.y};
 	}
-	Vec2 operator-(const int rhs) const
+	constexpr Vec2 operator-(const int rhs) const
 	{
 		return { x - rhs, y - rhs};
 	}
-	Vec2& operator-=(const Vec2& rhs)
+	constexpr Vec2& operator-=(const Vec2& rhs)
 	{
 		return *this = *this - rhs;
 	}
-	Vec2 operator*(const Vec2& rhs) const
+	constexpr Vec2 operator*(const Vec2& rhs) const
 	{
 		return {x * rhs.x, y * rhs.y};
 	}
-	Vec2 operator*(const int rhs) const
+	constexpr Vec2 operator*(const int rhs) const
 	{
 		return { x * rhs, y * rhs };
 	}
-	Vec2& operator*=(const Vec2& rhs)
+	constexpr Vec2& operator*=(const Vec2& rhs)
 	{
 		return *this = *this * rhs;
 	}
